aad5f82e2867725bae550c707e13a4cfe2c67ceb
tanvirabsar <tjoy4799@gmail.com>
2025-07-08 16:51:42 +0600

    add data on home landing page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,14 +5,25 @@ import Product from '../components/Product';
 
 const Home = () => {
     const products = useLoaderData();
-    console.log(products);
+   const uniqueByCategory = [];
+const seenCategories = new Set();
+
+for (let item of products) {
+  if (!seenCategories.has(item.category)) {
+    uniqueByCategory.push(item);
+    seenCategories.add(item.category);
+  }
+  if (uniqueByCategory.length === 8) break;
+}
+
+console.log(uniqueByCategory);
     return (
        <>
-       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-8'>
        {
-        products.map(product => <Product key={product._id} product={product}></Product>)
+        uniqueByCategory.map(product => <Product key={product._id} product={product}></Product>)
        }
-       </div>
+       </div> 
        </>
     );
 };
